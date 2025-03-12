b3153b6b9ad97d8e66abb6c37fbf827a075facad
SamarthPersonal <SamarthPersonal@SAMARTHs-MacBook-Pro.local>
2025-03-12 18:39:18 +0530

    added link to resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React from 'react';
+import './Hero.css';
+import profile_img from '../../assets/profile_img.svg';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Hero = () => {
   return (
@@ -10,11 +10,17 @@ const Hero = () => {
       <h1><span>I'm Samarth Chaudhary,</span> frontend developer from NST.</h1>
       <p>I am a Frontend Developer from NST. With a growth mindset, adaptability, and eagerness to learn, I embrace every challenge as an opportunity to evolve.</p>
       <div className="hero-action">
-        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <div className="hero-resume">My resume</div>
+        <div className="hero-connect">
+          <AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink>
+        </div>
+        <div className="hero-resume">
+          <a href="https://drive.google.com/file/d/1U9lXV5YebJdzeXFOck7S9_yk49diCMQl/view" target="_blank" rel="noopener noreferrer">
+            My Resume
+          </a>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
